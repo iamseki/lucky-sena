f1e5c9f51d352cd955aaffbf75b4abcecb9a7ecd
iamseki <chrisyuri_19@hotmail.com>
2020-12-26 11:06:13 -0300

    Move main entrypoint to cmd folder

--- run-example.sh
+++ run-example.sh
@@ -5,10 +5,10 @@ go run main.go --b=7 --e=1,2,3,4
 #go run main.go --f=mega_sena.xlsx
 
 ## COMPILE FOR LINUX
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build .
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o lucky-sena cmd/main.go 
 
 ## COMPILE FOR WINDOWS
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build .
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o lucky-sena.exe cmd/main.go
 
 ## COMPILE CUSTOM OUTPUT
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o randomNameBin .
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o randomNameBin cmd/main.go
