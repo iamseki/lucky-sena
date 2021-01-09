4b980aa72d9ee736dca160e6a4a2c5fdbc06b3aa
iamseki <chrisyuri_19@hotmail.com>
2021-01-08 21:46:18 -0300

    refactor: add cli folder and update docs

--- run-example.sh
+++ run-example.sh
@@ -11,10 +11,10 @@ go run main.go --b=7 --e=1,2,3,4
 go run main.go --f=mega_sena.xlsx
 
 ## COMPILE FOR LINUX
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o lucky-sena cmd/main.go 
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o lucky-sena-cli cmd/cli/main.go 
 
 ## COMPILE FOR WINDOWS
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o lucky-sena.exe cmd/main.go
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o lucky-sena-cli.exe cmd/cli/main.go
 
 ## COMPILE CUSTOM OUTPUT
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o randomNameBin cmd/main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o randomNameBin cmd/cli/main.go
