ff87268b602dcec789fdfbc1483a393b8d82603f
iamseki <chrisyuri_19@hotmail.com>
2021-01-23 21:45:49 -0300

    change mongo database name

--- infra/db/mongodb/helper.go
+++ infra/db/mongodb/helper.go
@@ -32,7 +32,7 @@ func newMongoConnection() *Mongo {
 	}
 
 	log.Printf("App is connected to MongoDB !")
-	return &Mongo{client, ctx, "lucky-sena"}
+	return &Mongo{client, ctx, "sena"}
 }
 
 func (m *Mongo) getCollection(name string) *mongo.Collection {
