9a82aa7833a487883410ff965e738b84f9fed5e3
iamseki <chrisyuri_19@hotmail.com>
2021-02-06 18:49:00 -0300

    docs: improve documentation

--- run-example.sh
+++ run-example.sh
@@ -5,16 +5,16 @@ go run ./... -v # To look for tests in all dirs and subdirs in this project
 go run ./services -v # To run tests only in services package
 
 ## Run with GO
-go run cmd/cli/generate-bet/*.go --b=7 --e=11,12,13,14,16,20,22,33,34,35,36,37,41,42,45,52,53,55,57,60 --c=3000
+go run main/cmd/generate-bet/*.go --b=7 --e=11,12,13,14,16,20,22,33,34,35,36,37,41,42,45,52,53,55,57,60 --p
 
-## Migrate xlsx to Mongo
-go run main.go --f=mega_sena.xlsx
+## COMPILE generate-bet FOR LINUX
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o generate-bet main/cmd/generate-bet/*.go 
 
-## COMPILE FOR LINUX
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o lucky-sena-cli cmd/cli/main.go 
+## COMPILE generate-bet FOR WINDOWS
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o generate-bet.exe main/cmd/generate-bet/*.go 
 
-## COMPILE FOR WINDOWS
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o lucky-sena-cli.exe cmd/cli/main.go
+## COMPILE scrapper FOR LINUX
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o scraper main/cmd/scraping/*.go
 
 ## COMPILE CUSTOM OUTPUT
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o randomNameBin cmd/cli/main.go
