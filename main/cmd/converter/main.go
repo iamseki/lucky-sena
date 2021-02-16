6f33fb6391f5f645ebd18d407ccc15e85ff104ac
iamseki <chrisyuri_19@hotmail.com>
2021-02-16 13:45:10 -0300

    chore: add some logs to csv converter proccess

--- main/cmd/converter/main.go
+++ main/cmd/converter/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"log"
 	"lucky-sena/main/factories"
 )
 
@@ -10,6 +11,16 @@ func main() {
 	flag.StringVar(&collection, "collection", "results", "collection to be read")
 	flag.StringVar(&collection, "c", "results", "shortly flag to what collection to be read")
 
-	c := factories.NewResultsBetToCsvConverter()
-	c.ConvertBetsToCsv()
+	if collection == "results" {
+		c := factories.NewResultsBetToCsvConverter()
+		c.ConvertBetsToCsv()
+		log.Println("Results collection converted into csv file successfully")
+	} else if collection == "bets" {
+		c := factories.NewMadeBetToCsvConverter()
+		c.ConvertBetsToCsv()
+		log.Println("Bets collection converted into csv file successfully")
+	} else {
+		log.Printf("Collection %v doesnt exist\n", collection)
+	}
+
 }
