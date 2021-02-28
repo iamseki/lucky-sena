dd4bb9c86a7b5d75ad0df70e3f0d859a179a5213
iamseki <chrisyuri_19@hotmail.com>
2021-02-27 23:04:09 -0300

    chore: add basic auth

--- main/http/iris-app/app.go
+++ main/http/iris-app/app.go
@@ -2,15 +2,29 @@ package irisapp
 
 import (
 	"lucky-sena/main/http/handlers"
+	"os"
 
 	"github.com/kataras/iris/v12"
+	"github.com/kataras/iris/v12/middleware/basicauth"
 )
 
+// NewIrisApp returns an instance of iris app
 func NewIrisApp() *iris.Application {
 	app := iris.New()
 
+	username := os.Getenv("API_BASIC_USERNAME")
+	password := os.Getenv("API_BASIC_PASSWORD")
+
+	auth := basicauth.New(basicauth.Config{
+		Users: map[string]string{
+			username: password,
+		},
+		Realm: "Authorization Required",
+	})
+
 	routesV1 := app.Party("/api/v1")
 	{
+		routesV1.Use(auth)
 		routesV1.Post("/generate", newGenerateBetsIrisAdapter(handlers.GenerateBetsHandle))
 	}
 
