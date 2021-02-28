bd0b041686a16ed7604cde4e1abccdb2be5f96a7
iamseki <chrisyuri_19@hotmail.com>
2021-02-27 23:33:12 -0300

    feat: ensure http server generate bet add bets into database also

--- main/http/iris-app/adapters.go
+++ main/http/iris-app/adapters.go
@@ -1,8 +1,11 @@
 package irisapp
 
 import (
+	"lucky-sena/domain"
 	"lucky-sena/infra/generator"
+	"lucky-sena/main/factories"
 	"lucky-sena/main/http/handlers"
+	"time"
 
 	"github.com/kataras/iris/v12"
 	"github.com/kataras/iris/v12/context"
@@ -24,6 +27,13 @@ func newGenerateBetsIrisAdapter(generateBetsFn handlers.GenerateBetsHandler) con
 		}
 		generatedBets := generateBetsFn(request.Bets, request.ExcludedBets)
 		response.Bets = generatedBets
+
+		addBetUseCase := factories.NewAddBetUseCase()
+		nextBetCodeUseCase := factories.NewAnalyzeBetUseCase("results")
+		code := nextBetCodeUseCase.NextBetCode()
+		for _, b := range generatedBets {
+			addBetUseCase.AddBet(domain.Bet{Numbers: b.Numbers, Code: code, Date: time.Now()})
+		}
 		ctx.JSON(response)
 	}
 }
