5eb381e97c157a9995cfb9836446d51054b710ad
iamseki <chrisyuri_19@hotmail.com>
2021-03-02 21:29:49 -0300

    chore: add cors and logger to api/v1 route

--- go.mod
+++ go.mod
@@ -4,9 +4,17 @@ go 1.15
 
 require (
 	github.com/360EntSecGroup-Skylar/excelize v1.4.1
+	github.com/CloudyKit/jet/v3 v3.0.0 // indirect
+	github.com/Joker/hpp v1.0.0 // indirect
+	github.com/dgraph-io/badger v1.6.0 // indirect
+	github.com/etcd-io/bbolt v1.3.3 // indirect
+	github.com/gavv/httpexpect v2.0.0+incompatible // indirect
 	github.com/gocolly/colly v1.2.0
 	github.com/gocolly/colly/v2 v2.1.0
-	github.com/kataras/iris/v12 v12.1.8
+	github.com/iris-contrib/blackfriday v2.0.0+incompatible // indirect
+	github.com/iris-contrib/middleware/cors v0.0.0-20210110101738-6d0a4d799b5d
+	github.com/kataras/iris v0.0.2
+	github.com/kataras/iris/v12 v12.2.0-alpha2
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
 	go.mongodb.org/mongo-driver v1.4.4
 )
--- main/http/iris-app/app.go
+++ main/http/iris-app/app.go
@@ -5,26 +5,31 @@ import (
 	"lucky-sena/main/http/handlers"
 	"os"
 
+	"github.com/iris-contrib/middleware/cors"
 	"github.com/kataras/iris/v12"
 	"github.com/kataras/iris/v12/middleware/basicauth"
+	"github.com/kataras/iris/v12/middleware/logger"
 )
 
 // NewIrisApp returns an instance of iris app
 func NewIrisApp() *iris.Application {
-	app := iris.Default()
+	app := iris.New()
+	app.Use(cors.Default())
+	iris.Default()
 
 	username := os.Getenv("API_BASIC_USERNAME")
 	password := os.Getenv("API_BASIC_PASSWORD")
 
-	auth := basicauth.New(basicauth.Config{
-		Users: map[string]string{
+	auth := basicauth.New(basicauth.Options{
+		Allow: basicauth.AllowUsers(map[string]string{
 			username: password,
-		},
+		}),
 		Realm: "Authorization Required",
 	})
 
 	app.Get("/hc", func(ctx iris.Context) { ctx.JSON(map[string]string{"health": "ok"}) })
 	routesV1 := app.Party("/api/v1")
+	routesV1.Use(logger.New())
 	{
 		routesV1.Use(auth)
 		routesV1.Post("/generate", newGenerateBetsIrisAdapter(handlers.GenerateBetsHandle))
