e1b9393fb8a4ea10170630948dfbbd0afc53281f
iamseki <chrisyuri_19@hotmail.com>
2021-03-02 21:14:55 -0300

    refactor: add retrying and reuse of connection

--- infra/db/mongodb/helper.go
+++ infra/db/mongodb/helper.go
@@ -8,6 +8,7 @@ import (
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 type Mongo struct {
@@ -16,24 +17,37 @@ type Mongo struct {
 	database string
 }
 
-func newMongoConnection() *Mongo {
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	//defer cancel()
+var mongoClient *mongo.Client
 
+func newMongoConnection() *Mongo {
 	mongoConn, declared := os.LookupEnv("MONGO_URI")
 	if !declared {
 		mongoConn = "mongodb://localhost:27017"
 	}
 
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoConn))
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	log.Println("New client connection at:", mongoConn)
-	return &Mongo{client, ctx, "sena"}
+	ctx := getMongoConnection(mongoConn)
+	return &Mongo{mongoClient, ctx, "sena"}
 }
 
 func (m *Mongo) getCollection(name string) *mongo.Collection {
 	return m.Client.Database(m.database).Collection(name)
+}
+
+func getMongoConnection(uri string) context.Context {
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+	if mongoClient == nil {
+		mongoClient, _ = mongo.Connect(ctx, options.Client().ApplyURI(uri), options.Client())
+		err := mongoClient.Ping(ctx, readpref.Primary())
+		if err != nil {
+			log.Println("Retrying to connect to mongodb")
+			mongoClient = nil
+			getMongoConnection(uri)
+		} else {
+			log.Println("Connected into mongodb: ", uri)
+		}
+		return ctx
+	}
+	log.Println("Using active mongo connection")
+	return ctx
 }
