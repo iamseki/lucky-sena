902b9f7712f234d28740328a78b774c3bdfb02ec
iamseki <chrisyuri_19@hotmail.com>
2021-03-03 22:54:42 -0300

    chore: add comments in Public resources at domain layer

--- domain/bet.go
+++ domain/bet.go
@@ -11,6 +11,7 @@ type Bet struct {
 	Date    time.Time `bson:"date" json:"date"`
 }
 
+// BetModel represents the bet returned from database
 type BetModel struct {
 	ID      string    `bson:"id" json:"id"`
 	Numbers []int     `bson:"numbers" json:"numbers"`
@@ -18,25 +19,31 @@ type BetModel struct {
 	Date    time.Time `bson:"date" json:"date"`
 }
 
+// BetCoast is the price of a single bet made in MegaSena
 const BetCoast = 4.5
 
+// BetCsvConverter expose the methods to be implemented by some code that knows how to do it
 type BetCsvConverter interface {
 	ConvertBetsToCsv() error
 }
 
+// BetAnalyzer expose the methods to be implemented by some code that knows how to do it
 type BetAnalyzer interface {
 	NextBetCode() (int, error)
 	IsBetAlreadyWon([]int) (bool, error)
 }
 
+// BetSaver expose the methods to be implemented by some code that knows how to do it
 type BetSaver interface {
 	AddBet(Bet) (BetModel, error)
 }
 
+// BetInserter expose the methods to be implemented by some code that knows how to do it
 type BetInserter interface {
 	InsertBets([]Bet) error
 }
 
+// BetFinder expose the methods to be implemented by some code that knows how to do it
 type BetFinder interface {
 	Find() ([]Bet, error)
 }
